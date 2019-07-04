5b40605fd9184accec1889ee2ce1ed23dccd643c
celeste99 <xueyuanxin94@gmail.com>
2019-07-03 21:17:09 -0400

    binarysearch

---
+++ findMin.java
@@ -0,0 +1,40 @@
+### 153  Find Minimum in Rotated Sorted Array
+public class findMin {
+	public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+}
+### 154  Find Minimum in Rotated Sorted Array
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else if (nums[mid] > nums[right]){
+                left = mid + 1;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+}
